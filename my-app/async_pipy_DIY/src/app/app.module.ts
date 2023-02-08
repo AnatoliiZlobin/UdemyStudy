dd0a90f50f9f305a97bf15af05e2e76fbc83098d
ANATOLII ZLOBIN <59967619+AnatoliiZlobin@users.noreply.github.com>
2023-02-08 20:49:42 +0100

    2023-02-08

---
+++ my-app/async_pipy_DIY/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppAsyncPipe } from './pipes/app-async.pipe';
+import { FooComponent } from './components/foo/foo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppAsyncPipe,
+    FooComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ my-app/async_pipy_DIY/src/app/components/foo/foo.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy } from '@angular/core';
+import { Component } from '@angular/core';
+import { delay, Observable, of} from 'rxjs';
+
+@Component({
+  selector: 'app-foo',
+  templateUrl: './foo.component.html',
+  styleUrls: ['./foo.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FooComponent {
+  /*user$ = of({name: 'dj', surname: 'kasperr'}).pipe(
+    delay(1000)
+  );*/
+  user$?: Observable<any>;
+  updateUser() {
+    this.user$ = of({name: 'Infinity', surname: 'Honda'}).pipe(
+      delay(1000)
+    );
+  }
+}
