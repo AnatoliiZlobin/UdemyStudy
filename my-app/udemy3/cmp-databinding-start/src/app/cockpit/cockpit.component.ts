1e5419ffc6d430f2a62efd3631cd6f96e98dfa18
ANATOLII ZLOBIN <59967619+AnatoliiZlobin@users.noreply.github.com>
2023-02-19 14:39:45 +0100

    2023-02-19

--- my-app/udemy3/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ my-app/udemy3/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -10,26 +10,25 @@ export class CockpitComponent implements OnInit {
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
   // newServerName = '';
-  newServerContent = '';
+  //newServerContent = '';
 
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-
-
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 }
--- my-app/udemy3/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ my-app/udemy3/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,14 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit,
+  Component, ContentChild,
+  DoCheck, ElementRef,
+  Input,
+  OnChanges, OnDestroy,
+  OnInit,
+  SimpleChanges, ViewChild,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,15 +16,54 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+{
   @Input('srvElement') element : {
     type: string,
     name: string,
     content: string
   };
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() name: string;
+  constructor() {
+    console.log('constructor called!');
+  }
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
+  ngOnChanges(changes:SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
   }
 
+  ngOnInit() {
+    console.log('ngOnInit called!');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!');
+  }
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!');
+  }
+  ngAfterContentChecked() {
+    console.log('ngAfterContentInit called!');
+  }
+  ngAfterViewInit() {
+    console.log('ngAfterContentInit called!');
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+    console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
+  ngAfterViewChecked() {
+    console.log('ngAfterContentInit called!');
+  }
+  ngOnDestroy() {
+    console.log('ngAfterContentInit called!');
+  }
 }
